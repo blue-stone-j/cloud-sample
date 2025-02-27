2255a1122fc9a0b7c2f75e245df20f8dab1cb191
blue-stone <qingshiji96@gmail.com>
2025-02-27 21:05:43 +0800

    simplify build

--- cbuild.sh
+++ cbuild.sh
@@ -1,1 +1,54 @@
-mkdir -p build && cd build && cmake .. && make install -j4 && cd ..
+
+# colorful output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+# rm -rf build &&
+# rm -rf install &&
+
+echo -e "${BLUE}Start working.${NC}"
+
+# set variable
+echo -e "${BLUE}Start setting variables.${NC}"
+PROJECT_NAME=cloud_sample
+PROJECT_NAME_U=$(echo "$PROJECT_NAME" | tr '[:lower:]' '[:upper:]')
+PROJECT_NAME_L=$(echo "$PROJECT_NAME" | tr '[:upper:]' '[:lower:]')
+
+CLOUD_SAMPLE_VERSION_MAJOR=1
+CLOUD_SAMPLE_VERSION_MINOR=0
+CLOUD_SAMPLE_VERSION_PATCH=0
+CLOUD_SAMPLE_VERSION=${CLOUD_SAMPLE_VERSION_MAJOR}.${CLOUD_SAMPLE_VERSION_MINOR}.${CLOUD_SAMPLE_VERSION_PATCH}
+
+BUILD_TEST=TRUE
+
+CMAKE_INSTALL_PREFIX=$(realpath $(dirname "${BASH_SOURCE[0]}"))/install
+echo -e "${GREEN}Set variables completed.${NC}"
+
+# build
+echo -e "${BLUE}Start building.${NC}"
+mkdir -p build &&
+cd build &&
+cmake -DCLOUD_SAMPLE_VERSION=${CLOUD_SAMPLE_VERSION} \
+      -DCMAKE_INSTALL_PREFIX=$CMAKE_INSTALL_PREFIX \
+      -DBUILD_TEST=$BUILD_TEST \
+      ..
+
+if [ ! $? -eq 0 ]; then
+  echo -e "${RED}Failed to build.${NC}"
+  exit
+fi
+
+make install -j8
+
+if [ ! $? -eq 0 ]; then
+  echo -e "${RED}Failed to build.${NC}"
+  exit
+fi
+
+echo -e "${GREEN}Build completed.${NC}"
+
+echo -e "${GREEN}All tasks have been done.${NC}"
+
---
+++ cmake/build_test.cmake
@@ -0,0 +1,9 @@
+if(BUILD_TEST)
+  foreach(CPP_FILE ${CPP_FILES})
+    # Get the filename without extension
+    get_filename_component(FILENAME_NO_EXT ${CPP_FILE} NAME_WE)
+
+    add_executable(${FILENAME_NO_EXT}_test test/${FILENAME_NO_EXT}_test.cpp src/${FILENAME_NO_EXT}.cpp)
+    target_link_libraries(${FILENAME_NO_EXT}_test ${common_lib})
+  endforeach()
+endif()
